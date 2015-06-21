baca4fd886839813e94a270b79971a4a89aef89a
Tim <timothy.w.holman@gmail.com>
2015-06-21 14:52:29 -0700

    site updates

--- js/uselessweb.js
+++ js/uselessweb.js
@@ -15,72 +15,55 @@ function uselessWebButton( button, popup ) {
 		['http://cant-not-tweet-this.com/',                           false, 7],
 		['http://eelslap.com/',                                       false, 5],
 		['http://www.staggeringbeauty.com/',                          false, 7],
-		// ['http://www.omfgdogs.com/',                                  false, 7], Down :(
  		['http://burymewithmymoney.com/',                             true,  7],
 		['http://www.fallingfalling.com/',                            true,  3],
 		['http://ducksarethebest.com/',                               false, 7],
 		['http://www.trypap.com/',                                    false, 4],
 		['http://www.republiquedesmangues.fr/',                       false, 7],
 		['http://www.movenowthinklater.com/',                         false, 6],
-		// ['http://www.hristu.net/',                                    true,  6], Down :(
 		['http://www.partridgegetslucky.com/',                        false, 6],
-		// ['http://hectorsalamanca.com/',                               true,  5], Big fat donate button
 		['http://www.rrrgggbbb.com/',                                 true,  7],
 		['http://beesbeesbees.com/',                                  false, 3],
 		['http://www.sanger.dk/',                                     true,  4],
-		// ['http://www.idiotic.com/',			                          true,  7], Down :(
-		// ['http://breakglasstosoundalarm.com/',                        false, 3], Down :(
 		['http://www.koalastothemax.com/',                            false, 7],
 		['http://www.everydayim.com/',                                false, 7],
 		['http://www.leduchamp.com/',                                 true,  7],
-		// ['http://www.geodu.de/',                                      false, 4], Down :(
 		['http://grandpanoclothes.com/',                              false, 6],
 		['http://www.haneke.net/',                                    false, 7],
-		// ['http://instantostrich.com/',                                true,  7], Lame banner
 		['http://r33b.net/',                                          true,  6],
 		['http://randomcolour.com/',                                  false, 6],
 		['http://cat-bounce.com/',                                    true,  6],
 		['http://cachemonet.com/',                                    true,  6],
 		['http://www.sadforjapan.com/',                               true,  5],
-		// ['http://isitchristmas.com/',                                 false, 5], Links out.
-		// ['http://infinitefrogs.com/',                                 false, 5],
 		['http://www.taghua.com/',                                    true,  5],
 		['http://chrismckenzie.com/',                                  true, 6],
 		['http://hasthelargehadroncolliderdestroyedtheworldyet.com/', false, 5],
 		['http://ninjaflex.com/',                                     false, 4],
 		['http://iloveyoulikeafatladylovesapples.com/',               true,  6],
 		['http://ihasabucket.com/',                                   false, 4],
-		// ['http://www.feedthehead.net/',                               true,  5], Iphone banner
 		['http://corndogoncorndog.com/',                              false, 4],
-		// ['http://www.oppositeofpoop.com/',                            false, 4], Down :(
 		['http://giantbatfarts.com/',                                 true,  4],
 		['http://www.ringingtelephone.com/',                          true,  6],
-		// ['http://chickenonaraft.com/',                                false, 4], Went down.
 		['http://www.pointerpointer.com/',                            false, 4],
 		['http://www.pleasedonate.biz/',                              false, 5],
 		['http://imaninja.com/',                                      false, 3],
 		['http://willthefuturebeaweso.me/',                           false, 3],
 		['http://salmonofcapistrano.com/',                            false, 3],
 		['http://www.ismycomputeron.com/',                            false, 5],
-		['http://www.ooooiiii.com/',                                  true,  3],
 		['http://www.wwwdotcom.com/',                                 false, 3],
 		['http://www.nullingthevoid.com/',                            true,  6],
 		['http://www.muchbetterthanthis.com/',                        true,  5],
 		['http://www.ouaismaisbon.ch/',                               true,  3],
-		// ['http://cutmeoffmidfunk.com/',                               false, 4], Down now
 		['http://iamawesome.com/',                                    false, 3],
 		['http://www.pleaselike.com/',                                false, 7],
 		['http://crouton.net/',                                       false, 3],
 		['http://corgiorgy.com/',                                     false, 3],
-		// ['http://www.clicktoremove.com/',                             false, 3], Lame external link
-		// ['http://dastardlydigitofdestiny.com/',                       false, 3], Sloooow
 		['http://www.electricboogiewoogie.com/',                      true,  6],
 		['http://www.nelson-haha.com/',                               false, 6],
 		['http://www.wutdafuk.com/',                                  false, 6],
 		['http://unicodesnowmanforyou.com/',                          false, 3],
 		['http://tencents.info/',                                     false, 3],
 		['http://intotime.com/',                                      true,  4],
-		// ['http://haveahairymovember.com',                             false, 4], Broken :(
 		['http://leekspin.com/',                                      true,  3],
 		['http://minecraftstal.com/',                                 false, 4],
 		['http://www.riddlydiddly.com/',                              false, 5],
@@ -95,7 +78,8 @@ function uselessWebButton( button, popup ) {
 		['http://isitwhite.com/',                                     false, 1],
 		['http://noot.space/',                                        false, 1],
 		['http://tomsdog.com/',                                       false, 1],
-		// ['http://goat.com/',                                           true, 1],	
+		['http://hardcoreprawnlawn.com/',                             false, 1],
+		['http://www.omfgdogs.com/',                                  false, 7],
 	];
 
 	var sites = null;
